e00e05202fb51a79d1546a5b135d1ba78c95fa37
Alban Linard <alban@linard.fr>
2017-11-17 14:06:41 +0100

    pnmc runs correctly.

--- pnmc/pnmc.py
+++ pnmc/pnmc.py
@@ -28,7 +28,7 @@ printerr = lambda *x : print(*x, file=sys.stderr)
 
 os.environ['LD_LIBRARY_PATH']="/usr/local/lib:/usr/local/lib64:" + os.environ.get('LD_LIBRARY_PATH', default="")
 
-printerr(os.environ)
+# printerr(os.environ)
 
 ##################################################################################
 
@@ -51,27 +51,26 @@ with open('iscolored', 'r') as is_colored_file:
 model_name = os.environ['BK_INPUT']
 model_file = os.path.join(os.path.abspath(os.getcwd()), 'model.pnml')
 orders_dir = '/home/mcc/BenchKit/bin/orders'
-pnmc = '/home/mcc/BenchKit/bin/pnmc'
-caesar = '/home/mcc/BenchKit/bin/caesar.sdd'
+pnmc = 'pnmc'
+caesar = 'caesar.sdd'
 results_file = 'results.json'
 model_bpn_file = os.path.join(os.path.abspath(os.getcwd()), 'model.bpn')
 
 ##################################################################################
 
 # Is it a known model?
-known = not os.path.isfile(os.path.join(os.path.abspath(os.getcwd()), 'NewModel'))
-printerr('Known', known)
+# known = not os.path.isfile(os.path.join(os.path.abspath(os.getcwd()), 'NewModel'))
+# printerr('Known', known)
 
 ##################################################################################
 
 # First check if we have a model with a NUPN tool specific section
 try:
-
   nupn.parse(model_file, model_bpn_file)
 
   # Common command line arguments for both known and unknown models.
   cli = [caesar, '--show-nb-states', model_bpn_file]
-  printerr(cli)
+  # printerr(cli)
   output = subprocess.check_output(cli)
 
   # Export results to BenchKit
@@ -81,53 +80,37 @@ try:
 
 # Not a NUPN model or caesar.sdd failed.
 except Exception as e:
+  # printerr(e)
 
-  printerr(e)
-
+  # subprocess.call([pnmc, "--help"], stdout=sys.stderr)
   # Common command line arguments for both known and unknown models.
   cli = [ pnmc
         , '--cache-size=hom:16000000,sum:16000000,inter:8000000,diff:8000000'
-        # , '--json=stats'
-        , '--input=pnml'
+        , '--json=results'
+        , '--format=pnml'
         , '--count-tokens'
+        , '--order-force'
         , model_file]
 
-  if known:
+  # Launch pnmc
+  # printerr(cli)
+  ret = subprocess.call(cli, stdout=sys.stderr)
 
-    # Construct command line arguments for known models
-    order_file = os.path.join(orders_dir, model_name + '.pnml.json')
-    if os.path.isfile(order_file):
-      cli.append('--order-load=' + order_file)
-    else:
-      cli.append('--order-force')
+  # Check if pnmc correctly finished.
+  if ret != 0:
+    printerr('pnmc failed with return code ' + str(ret))
+    print('CANNOT COMPUTE')
+    sys.exit(0)
 
-    # Launch pnmc.
-    printerr(cli)
-    subprocess.call(cli, stdout=sys.stderr)
+while not os.path.exists(results_file):
+  time.sleep(1/10) # 1/10 second
 
-  else: # Unknown
+# Export results to BenchKit
+with open(results_file, 'r') as f:
+  results = json.load(f)
+  nb_states = results['pnmc']['states']
+  marking = results['pnmc']['max number of tokens per marking']
+  place  = results['pnmc']['max number of tokens in a place']
+  print_result(nb_states, marking, place)
 
-    cli.append('--order-force')
-
-    # Launch pnmc
-    printerr(cli)
-    ret = subprocess.call(cli, stdout=sys.stderr)
-
-    # Check if pnmc correctly finished.
-    if ret != 0:
-      printerr('pnmc failed with return code ' + str(ret))
-      print('CANNOT COMPUTE')
-      sys.exit(0)
-
-    while not os.path.exists(results_file):
-      time.sleep(1/10) # 1/10 second
-
-  # Export results to BenchKit
-  with open(results_file, 'r') as f:
-    results = json.load(f)
-    nb_states = results['pnmc']['states']
-    marking = results['pnmc']['max number of tokens per marking']
-    place  = results['pnmc']['max number of tokens in a place']
-    print_result(nb_states, marking, place)
-
-  sys.exit(0)
+sys.exit(0)
