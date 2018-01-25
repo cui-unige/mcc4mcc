079e5e7d153e516b2c8685996351d7373a71795c
Alban Linard <alban@linard.fr>
2018-01-25 14:26:04 +0100

    Compute results for machine learning algorithms.

--- mcc/extract.py
+++ mcc/extract.py
@@ -1,5 +1,22 @@
 #! /usr/bin/env python3
 
+import argparse
+import csv
+import json
+import logging
+import os
+import re
+import statistics
+import pandas
+from tqdm                   import tqdm
+from sklearn.neighbors      import KNeighborsClassifier
+from sklearn.ensemble       import BaggingClassifier
+from sklearn.naive_bayes    import GaussianNB
+from sklearn.svm            import SVC, LinearSVC
+from sklearn                import tree
+from sklearn.ensemble       import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+
 CHARACTERISTICS = [
     "Id",
     "Type",
@@ -50,9 +67,9 @@ RESULTS = [
 def value_of (x):
     if x in [ "True", "Yes", "OK" ]:
         return True
-    elif x in [ "False", "None" ]:
+    if x in [ "False", "None" ]:
         return False
-    elif x == "Unknown":
+    if x == "Unknown":
         return None
     try:
         return int (x)
@@ -64,16 +81,76 @@ def value_of (x):
         pass
     return x
 
-if __name__ == "__main__":
+def knn(train_X, train_Y, test_X, test_Y):
+    clf = KNeighborsClassifier(
+        n_neighbors=10, weights='distance', algorithm='brute'
+    )
+    clf.fit(train_X, train_Y)
+    return clf.score(test_X, test_Y)
 
-    import argparse
-    import csv
-    import io
-    import json
-    import logging
-    import os
-    import re
-    from tqdm import tqdm
+def bagging_knn(train_X, train_Y, test_X, test_Y):
+    # Bagging contains n_estimators of
+    # KNeighborsClassifier(n_neighbors=10, weights='distance', n_jobs=4).
+    # Every knn-classifier will take the half of the training set to learn.
+    bagging = BaggingClassifier(
+        KNeighborsClassifier(
+            n_neighbors=10, weights='distance', algorithm='brute'
+        ), max_samples=0.5, max_features=1, n_estimators=10
+    )
+    # Train the model:
+    bagging.fit(train_X, train_Y)
+    # Return the accuracy of guessing:
+    return bagging.score(test_X, test_Y)
+
+def gaussian_naive_bayes(train_X, train_Y, test_X, test_Y):
+    # http://scikit-learn.org/stable/modules/naive_bayes.html
+    gnb = GaussianNB()
+    gnb.fit(train_X, train_Y)
+    return gnb.score(test_X, test_Y)
+
+def svm(train_X, train_Y, test_X, test_Y):
+    # http://scikit-learn.org/stable/modules/svm.html
+    # Support Vector Classifier with rbf kernel.
+    clf = SVC()
+    clf.fit(train_X, train_Y)
+    return clf.score(test_X, test_Y)
+
+def linear_svm(train_X, train_Y, test_X, test_Y):
+    # http://scikit-learn.org/stable/modules/svm.html
+    # Linear Support Vector classifier with rbf kernel.
+    clf = LinearSVC()
+    clf.fit(train_X, train_Y)
+    return clf.score(test_X, test_Y)
+
+def decision_tree(train_X, train_Y, test_X, test_Y):
+    clf = tree.DecisionTreeClassifier()
+    clf.fit(train_X, train_Y)
+    return clf.score(test_X, test_Y)
+
+def random_forest(train_X, train_Y, test_X, test_Y):
+    clf = RandomForestClassifier(n_estimators=10, max_features=None)
+    clf = clf.fit(train_X, train_Y)
+    return clf.score(test_X, test_Y)
+
+def neural_net(train_X, train_Y, test_X, test_Y):
+    # http://scikit-learn.org/stable/modules/neural_networks_supervised.html
+    # Simple multi-layer neural net using Newton solver.
+    nn = MLPClassifier(solver='lbfgs')
+    nn.fit(train_X, train_Y)
+    return nn.score(test_X, test_Y)
+
+algorithms = {
+    "KNN": knn,
+    "Bagging+knn": bagging_knn,
+    "Gaussian Naive Bayes": gaussian_naive_bayes,
+    "Support Vector Machine": svm,
+    "Linear Support Vector Machine": linear_svm,
+    "Neural Net": neural_net,
+    "Decision Tree": decision_tree,
+    "Random Forest": random_forest,
+}
+
+if __name__ == "__main__":
 
     parser = argparse.ArgumentParser (
         description = "..." # FIXME
@@ -106,23 +183,21 @@ if __name__ == "__main__":
         dest    = "learned",
         default = os.getcwd () + "/learned.json",
     )
-    parser.add_argument (
-        "-v", "--verbose",
-        help   = "input directory or archive",
-        action = "store_true",
-        dest   = "verbose",
+    parser.add_argument(
+        "--iterations",
+        help    = "Number of iterations",
+        type    = int,
+        dest    = "iterations",
+        default = 100,
     )
     arguments = parser.parse_args ()
-    if arguments.verbose:
-        logging.basicConfig (level = logging.INFO)
-    else:
-        logging.basicConfig (level = logging.WARNING)
+    logging.basicConfig (level = logging.INFO)
 
     techniques = {}
 
     characteristics = {}
     logging.info (f"Reading model characteristics from '{arguments.characteristics}'.")
-    with tqdm (total = sum (1 for line in open (arguments.characteristics))) as counter:
+    with tqdm (total = sum (1 for line in open (arguments.characteristics)) - 1) as counter:
         with open (arguments.characteristics) as data:
             data.readline () # skip the title line
             reader = csv.reader (data)
@@ -142,7 +217,7 @@ if __name__ == "__main__":
 
     results = {}
     logging.info (f"Reading mcc results from '{arguments.results}'.")
-    with tqdm (total = sum (1 for line in open (arguments.results))) as counter:
+    with tqdm (total = sum (1 for line in open (arguments.results)) - 1) as counter:
         with open (arguments.results) as data:
             data.readline () # skip the title line
             reader = csv.reader (data)
@@ -254,7 +329,74 @@ if __name__ == "__main__":
                             counter.update (1)
                     s = sorted (subsubresults.items (), key = lambda e: (e [1] ["time"], e [1] ["memory"]))
                     known_i ["sorted"] = [ { "tool": x [0], "time": x [1] ["time"], "memory": x [1] ["memory"] } for x in s]
+                    rank = 0
+                    for x in known_i ["sorted"]:
+                        rank += 1
+                        for tool, years in tools.items ():
+                            for year, entry in years.items ():
+                                if year == tool_year [tool]:
+                                    entry ["Rank"] = rank
                 s = sorted (subresults.items (), key = lambda e: (- e [1] ["count"], e [1] ["time"], e [1] ["memory"]))
                 known_m ["sorted"] = [ { "tool": x [0], "count": x [1] ["count"], "time": x [1] ["time"], "memory": x [1] ["memory"] } for x in s]
     with open ("known.json", "w") as output:
         json.dump (known, output)
+
+    logging.info (f"Analyzing learned data.")
+    learned     = []
+    next_id     = 10
+    translation = {
+        False: -1,
+        None : 0,
+        True : 1,
+    }
+    def translate (x):
+        global next_id
+        if x is None:
+            return 0
+        if isinstance (x, (bool, str)):
+            if x not in translation:
+                translation [x] = next_id + 1
+                next_id        += 1
+            return translation [x]
+        else:
+            return x
+    with tqdm (total = len (results)) as counter:
+        for _, entry in results.items ():
+            if entry ["Year"] == tool_year [entry ["Tool"]]:
+                cp = {}
+                for key, value in entry.items ():
+                    if key != "Id" and key != "Model Id" and key != "Year" and key != "Instance":
+                        cp [key] = translate (value)
+                learned.append (cp)
+            counter.update (1)
+    with open ("learned.translation.json", "w") as output:
+        json.dump (translation, output)
+
+    # Convert this dict into dataframe:
+    df = pandas.DataFrame (learned)
+    # Select only the best tools:
+    df = df.loc [df ["Rank"] == 1]
+    # Remove the Tool columns from X.
+    X = df.drop ("Tool", 1)
+    Y = df ["Tool"]
+    # Compute efficiency for each algorithm:
+    for algorithm in algorithms:
+        subresults = []
+        print(f"  Algorithm: {algorithm}")
+        for _ in tqdm (range (arguments.iterations)):
+            n             = X.shape [0]
+            training_size = int (n * 0.75)
+            training_X    = X.sample (training_size)
+            training_Y    = Y [training_X.index]
+            # Remove the training points:
+            tmp_X         = pandas.concat ([X, training_X]).drop_duplicates (keep = False)
+            # Get the test points:
+            test_X        = tmp_X.sample (min (int (n * 0.25), tmp_X.shape [0]))
+            test_Y        = Y [test_X.index]
+            subresults.append (algorithms.get (algorithm) (training_X, training_Y, test_X, test_Y))
+        print(f"    Min     : {min                (subresults)}")
+        print(f"    Max     : {max                (subresults)}")
+        print(f"    Mean    : {statistics.mean    (subresults)}")
+        print(f"    Median  : {statistics.median  (subresults)}")
+        print(f"    Stdev   : {statistics.stdev   (subresults)}")
+        print(f"    Variance: {statistics.variance(subresults)}")
