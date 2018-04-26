25cd862496dc02e6ce1079493abb72528137e7e9
Alban Linard <alban@linard.fr>
2018-04-26 14:20:36 +0200

    Fix benchkit head script as asked by Fabrice.

--- BenchKit_head.sh
+++ BenchKit_head.sh
@@ -1,12 +1,34 @@
 #! /usr/bin/env bash
 
-if [ ! -z "${BK_CHEAT+x}" ]
-then
-  cheat="--cheat"
-fi
-
-python3 \
-  -m mcc4mcc \
-  --data="/usr/share/mcc4mcc" \
-  run \
-  ${cheat}
+case "${BK_TOOL}" in
+  "mcc4mcc-full")
+    python3 \
+      -m mcc4mcc \
+      --data="/usr/share/mcc4mcc" \
+      run \
+      --prefix="87bd5343c30a279e"
+  ;;
+  "mcc4mcc-structural")
+    python3 \
+      -m mcc4mcc \
+      --data="/usr/share/mcc4mcc" \
+      run \
+      --prefix="7e556e9247727f60"
+  ;;
+  "irma4mcc-full")
+    python3 \
+      -m mcc4mcc \
+      --data="/usr/share/mcc4mcc" \
+      run \
+      --prefix="87bd5343c30a279e" \
+      --cheat
+  ;;
+  "irma4mcc-structural")
+    python3 \
+      -m mcc4mcc \
+      --data="/usr/share/mcc4mcc" \
+      run \
+      --prefix="7e556e9247727f60" \
+      --cheat
+  ;;
+esac
